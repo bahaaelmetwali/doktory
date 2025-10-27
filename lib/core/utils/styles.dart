9901a1f13529f1b1dc75e494d41696ed3bb39bc9
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-10-27 22:26:36 +0300

    screen_util_and_fonts_added

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class Styles {
+  static final TextStyle textStyle28ExtraLight = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 28.sp,
+    fontWeight: FontWeight.w200,
+  );
+
+  static final TextStyle textStyle20Light = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w300,
+  );
+
+  static final TextStyle textStyle14Regular = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle textStyle10Regular = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle textStyle16Medium = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle textStyle18SemiBold = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle textStyle20SemiBold = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle textStyle32SemiBold = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle textStyle12SemiBold = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle textStyle28Bold = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 28.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle textStyle32ExtraBold = TextStyle(
+    fontFamily: 'Cairo',
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w800,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:doktory/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:doktory/features/splash/presentation/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+void main() {
   runApp(const Doktory());
 }
 
@@ -15,15 +11,12 @@ class Doktory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Doktory',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Welcome to Doktory!'),
-        ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const SplashScreen(),
       ),
     );
   }
