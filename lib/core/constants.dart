be11646eec559128d323f220252e25cfabcbf84a
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-10-28 22:07:48 +0300

    App_Colors_Added

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  // الألوان الرئيسية
-  static const Color primary = Color(0xff0C5137);
-  static const Color error = Color(0xffFF3B30);
-  static const Color scaffold = Color(0xffF2F2F2);
+  static const Color primary = Color(0xff79affe);
 
-  // لو عاوز تضيف ألوان إضافية
-  static const Color accent = Color(0xffFFC107);
+  static const Color accent = Color(0xff0066CC);
+
+  static const Color success = Color(0xff4CAF50);
+  static const Color error = Color(0xffFF3B30);
+
+  static const Color scaffold = Color(0xffF2F6FC);
+
   static const Color darkText = Color(0xff1C1C1C);
+
   static const Color lightText = Color(0xffFFFFFF);
+  static const mainLogo = 'assets/logos/AppLogo.png';
 }
