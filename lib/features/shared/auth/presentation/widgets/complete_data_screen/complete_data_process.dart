a2de9a6a513a37a9db974d507d70eb9d5d6ec559
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-11-05 21:16:21 +0200

    Refactor : complete data screen body refactored

---
+++ lib/features/shared/auth/presentation/widgets/complete_data_screen/complete_data_process.dart
@@ -0,0 +1,84 @@
+import 'package:doktory/core/utils/service_locator.dart';
+import 'package:doktory/core/widgets/custom_button.dart';
+import 'package:doktory/core/widgets/custom_loading_indicator.dart';
+import 'package:doktory/core/widgets/show_custom_snack_bar.dart';
+import 'package:doktory/features/shared/auth/data/models/user_model.dart';
+import 'package:doktory/features/shared/auth/presentation/cubits/complete_user_data_cubit/complete_user_data_cubit.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CompleteDataProcess extends StatelessWidget {
+  const CompleteDataProcess({
+    super.key,
+    required this.formKey,
+    required this.nameController,
+    required this.phoneController,
+    required this.selectedGovernorate,
+    required this.selectedRole,
+    this.selectedSpecialization,
+    this.selectedLocation,
+    this.address,
+  });
+  final GlobalKey<FormState> formKey;
+  final TextEditingController nameController;
+  final TextEditingController phoneController;
+  final String? selectedGovernorate;
+  final String? selectedRole;
+  final String? selectedSpecialization;
+  final LatLng? selectedLocation;
+  final String? address;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<CompleteUserDataCubit, CompleteUserDataState>(
+      listener: (context, state) {
+        if (state is CompleteUserDataSuccess) {
+          showCustomSnackBar(context, message: 'تم تسجيل الدخول');
+        } else if (state is CompleteUserDataFailure) {
+          showCustomSnackBar(context, message: state.message, isError: true);
+        }
+      },
+      builder: (context, state) {
+        if (state is CompleteUserDataLoading) {
+          return CustomLoadingIndicator();
+        } else {
+          return Center(
+            child: CustomButton(
+              text: 'تسجيل الدخول',
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  final firebaseAuth = getIt<FirebaseAuth>();
+                  final currentUser = firebaseAuth.currentUser!;
+
+                  final userModel = UserModel(
+                    uid: currentUser.uid,
+                    email: currentUser.email ?? '',
+                    name: nameController.text.trim(),
+                    phone: phoneController.text.trim(),
+                    governorate: selectedGovernorate,
+                    role: selectedRole,
+                    specialization: selectedRole == 'دكتور'
+                        ? selectedSpecialization
+                        : null,
+                    address: selectedRole == 'دكتور' ? address : null,
+                    latitude: selectedRole == 'دكتور'
+                        ? selectedLocation?.latitude
+                        : null,
+                    longitude: selectedRole == 'دكتور'
+                        ? selectedLocation?.longitude
+                        : null,
+                  );
+                  context.read<CompleteUserDataCubit>().completeUserData(
+                    user: userModel,
+                  );
+                }
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+}
--- lib/features/shared/auth/presentation/widgets/complete_data_screen/complete_data_screen_body.dart
+++ lib/features/shared/auth/presentation/widgets/complete_data_screen/complete_data_screen_body.dart
@@ -2,19 +2,13 @@ import 'package:doktory/core/constants.dart';
 import 'package:doktory/core/utils/cache_helper.dart';
 import 'package:doktory/core/utils/service_locator.dart';
 import 'package:doktory/core/utils/styles.dart';
-import 'package:doktory/core/widgets/custom_button.dart';
-import 'package:doktory/core/widgets/custom_loading_indicator.dart';
-import 'package:doktory/core/widgets/show_custom_snack_bar.dart';
-import 'package:doktory/features/shared/auth/data/models/user_model.dart';
-import 'package:doktory/features/shared/auth/presentation/cubits/complete_user_data_cubit/complete_user_data_cubit.dart';
+import 'package:doktory/features/shared/auth/presentation/widgets/complete_data_screen/complete_data_process.dart';
 import 'package:doktory/features/shared/auth/presentation/widgets/complete_data_screen/get_location_section.dart';
 import 'package:doktory/features/shared/auth/presentation/widgets/complete_data_screen/governorate_dropdown.dart';
 import 'package:doktory/features/shared/auth/presentation/widgets/complete_data_screen/name_and_phone_text_fields_section.dart';
 import 'package:doktory/features/shared/auth/presentation/widgets/complete_data_screen/pick_image_section.dart';
 import 'package:doktory/features/shared/auth/presentation/widgets/complete_data_screen/specializations_dropdown.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -102,7 +96,7 @@ class _CompleteDataScreenBodyState extends State<CompleteDataScreenBody> {
                     )
                   : SizedBox.shrink(),
               SizedBox(height: 10.h),
-              CompleteDataSource(
+              CompleteDataProcess(
                 formKey: _formKey,
                 nameController: nameController,
                 phoneController: phoneController,
@@ -116,80 +110,6 @@ class _CompleteDataScreenBodyState extends State<CompleteDataScreenBody> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class CompleteDataSource extends StatelessWidget {
-  const CompleteDataSource({
-    super.key,
-    required this.formKey,
-    required this.nameController,
-    required this.phoneController,
-    required this.selectedGovernorate,
-    required this.selectedRole,
-    this.selectedSpecialization,
-    this.selectedLocation,
-    this.address,
-  });
-  final GlobalKey<FormState> formKey;
-  final TextEditingController nameController;
-  final TextEditingController phoneController;
-  final String? selectedGovernorate;
-  final String? selectedRole;
-  final String? selectedSpecialization;
-  final LatLng? selectedLocation;
-  final String? address;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<CompleteUserDataCubit, CompleteUserDataState>(
-      listener: (context, state) {
-        if (state is CompleteUserDataSuccess) {
-          showCustomSnackBar(context, message: 'تم تسجيل الدخول');
-        } else if (state is CompleteUserDataFailure) {
-          showCustomSnackBar(context, message: state.message, isError: true);
-        }
-      },
-      builder: (context, state) {
-        if (state is CompleteUserDataSuccess) {
-          return CustomLoadingIndicator();
-        } else {
-          return Center(
-            child: CustomButton(
-              text: 'تسجيل الدخول',
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  final firebaseAuth = getIt<FirebaseAuth>();
-                  final currentUser = firebaseAuth.currentUser!;
-
-                  final userModel = UserModel(
-                    uid: currentUser.uid,
-                    email: currentUser.email ?? '',
-                    name: nameController.text.trim(),
-                    phone: phoneController.text.trim(),
-                    governorate: selectedGovernorate,
-                    role: selectedRole,
-                    specialization: selectedRole == 'دكتور'
-                        ? selectedSpecialization
-                        : null,
-                    address: selectedRole == 'دكتور' ? address : null,
-                    latitude: selectedRole == 'دكتور'
-                        ? selectedLocation?.latitude
-                        : null,
-                    longitude: selectedRole == 'دكتور'
-                        ? selectedLocation?.longitude
-                        : null,
-                  );
-                  context.read<CompleteUserDataCubit>().completeUserData(
-                    user: userModel,
-                  );
-                }
-              },
-            ),
-          );
-        }
-      },
     );
   }
 }
