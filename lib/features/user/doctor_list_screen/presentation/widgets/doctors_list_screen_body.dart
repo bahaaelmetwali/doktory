1b68ee40d8891ddf1f6cde78b336f6b3db67c826
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-11-07 22:10:02 +0200

    feat: row of categories added

--- lib/features/user/doctor_list_screen/presentation/widgets/doctors_list_screen_body.dart
+++ lib/features/user/doctor_list_screen/presentation/widgets/doctors_list_screen_body.dart
@@ -1,3 +1,5 @@
+import 'package:doktory/core/constants.dart';
+import 'package:doktory/core/utils/styles.dart';
 import 'package:doktory/core/widgets/home_app_bar.dart';
 import 'package:doktory/features/user/doctor_list_screen/presentation/widgets/welcome_banner_section.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +15,34 @@ class DoctorsListScreenBody extends StatelessWidget {
         HomeAppBar(),
         SizedBox(height: 20.h),
         WelcomeBannerSection(),
+        SizedBox(height: 10.h),
+        Padding(
+          padding: EdgeInsets.all(8.r),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text('التخصصات', style: Styles.textStyle20SemiBold),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'عرض الكل',
+                      style: Styles.textStyle14SemiBold.copyWith(
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ListView.builder(
+                itemBuilder: (context, int) {
+                  return Container();
+                },
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
--- lib/features/user/doctor_list_screen/presentation/widgets/welcome_banner_section.dart
+++ lib/features/user/doctor_list_screen/presentation/widgets/welcome_banner_section.dart
@@ -8,47 +8,50 @@ class WelcomeBannerSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150.h,
-      width: 355.w,
-      decoration: BoxDecoration(
-        color: AppColors.primary,
-        borderRadius: BorderRadius.circular(12.r),
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            bottom: 0,
-            left: 0.h,
-            child: SizedBox(
-              height: 160.h,
-              child: Image.asset('assets/logos/doctor.png'),
+    return Padding(
+      padding: EdgeInsets.all(8.r),
+      child: Container(
+        height: 150.h,
+        width: 355.w,
+        decoration: BoxDecoration(
+          color: AppColors.primary,
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              left: 0.h,
+              child: SizedBox(
+                height: 160.h,
+                child: Image.asset('assets/logos/doctor.png'),
+              ),
             ),
-          ),
 
-          Positioned(
-            top: 40.h,
-            right: 5.w,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'مرحبًا بك في دكتوري !',
-                  style: Styles.textStyle20SemiBold.copyWith(
-                    color: Colors.white,
+            Positioned(
+              top: 40.h,
+              right: 5.w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'مرحبًا بك في دكتوري !',
+                    style: Styles.textStyle20SemiBold.copyWith(
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  'صحتك تهمنا، استشر طبيبك في أي وقت',
-                  style: Styles.textStyle14SemiBold.copyWith(
-                    color: Colors.white,
+                  const SizedBox(height: 5),
+                  Text(
+                    'صحتك تهمنا، استشر طبيبك في أي وقت',
+                    style: Styles.textStyle14SemiBold.copyWith(
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
