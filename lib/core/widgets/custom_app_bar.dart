9e14fea80a2928d89e0b0c3ee456573ebf245293
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-11-09 15:06:33 +0200

    feat:  custom app  bar added

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:doktory/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+    required this.onTap,
+    required this.headingText,
+  });
+  final void Function()? onTap;
+  final String headingText;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(top: 16.h, right: 8.w, left: 8.w),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: onTap,
+                  child: SvgPicture.asset(
+                    'assets/icons/arrow-right.svg',
+                    height: 20.h,
+                    width: 30.w,
+                  ),
+                ),
+                Spacer(),
+                Text(headingText, style: Styles.textStyle20SemiBold),
+                Spacer(),
+                SizedBox(width: 30.w, height: 20.h),
+              ],
+            ),
+          ),
+        ),
+        Container(width: double.infinity, height: 20.h, color: Colors.white),
+      ],
+    );
+  }
+}
--- lib/features/user/home/presentation/widgets/all_doctors_screen_body.dart
+++ lib/features/user/home/presentation/widgets/all_doctors_screen_body.dart
@@ -1,5 +1,7 @@
 // all_doctors_screen_body.dart
+import 'package:doktory/core/constants.dart';
 import 'package:doktory/core/utils/styles.dart';
+import 'package:doktory/core/widgets/custom_app_bar.dart';
 import 'package:doktory/features/user/home/presentation/cubits/all_doctors/all_doctors_cubit.dart';
 import 'package:doktory/features/user/home/presentation/widgets/doctor_card.dart';
 import 'package:flutter/material.dart';
@@ -11,40 +13,53 @@ class AllDoctorsScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        BlocBuilder<AllDoctorsCubit, AllDoctorsState>(
-          builder: (context, state) {
-            if (state is AllDoctorsLoading) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is AllDoctorsError) {
-              return Center(child: Text(state.message));
-            } else if (state is AllDoctorsLoaded) {
-              final doctors = state.doctors;
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Column(
+        children: [
+          CustomAppBar(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            headingText: 'كل الاطباء',
+          ),
+          BlocBuilder<AllDoctorsCubit, AllDoctorsState>(
+            builder: (context, state) {
+              if (state is AllDoctorsLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(color: AppColors.primary),
+                );
+              } else if (state is AllDoctorsError) {
+                return Center(child: Text(state.message));
+              } else if (state is AllDoctorsLoaded) {
+                final doctors = state.doctors;
 
-              if (doctors.isEmpty) {
-                return Center(
-                  child: Text(
-                    'لا يوجد أطباء حالياً',
-                    style: Styles.textStyle16Medium,
+                if (doctors.isEmpty) {
+                  return Center(
+                    child: Text(
+                      'لا يوجد أطباء حالياً',
+                      style: Styles.textStyle16Medium,
+                    ),
+                  );
+                }
+
+                return Expanded(
+                  child: ListView.builder(
+                    padding: EdgeInsets.all(8.r),
+                    itemCount: doctors.length,
+                    itemBuilder: (context, index) {
+                      final doctor = doctors[index];
+                      return DoctorCard(doctor: doctor);
+                    },
                   ),
                 );
+              } else {
+                return const SizedBox();
               }
-
-              return ListView.builder(
-                padding: EdgeInsets.all(8.r),
-                itemCount: doctors.length,
-                itemBuilder: (context, index) {
-                  final doctor = doctors[index];
-                  return DoctorCard(doctor: doctor);
-                },
-              );
-            } else {
-              return const SizedBox();
-            }
-          },
-        ),
-      ],
+            },
+          ),
+        ],
+      ),
     );
   }
 }
