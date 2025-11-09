31753c75394885f291798d8ea4a14262cf73ed68
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-11-09 04:02:06 +0200

    refactor: doctor card refactored

--- lib/features/user/doctor_list_screen/presentation/widgets/doctors_list.dart
+++ lib/features/user/doctor_list_screen/presentation/widgets/doctors_list.dart
@@ -1,5 +1,6 @@
 import 'package:doktory/core/constants.dart';
 import 'package:doktory/core/utils/styles.dart';
+import 'package:doktory/features/user/doctor_list_screen/presentation/widgets/doctor_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -33,71 +34,7 @@ class DoctorsList extends StatelessWidget {
             shrinkWrap: true,
             itemCount: 10,
             itemBuilder: (context, index) {
-              return Padding(
-                padding: EdgeInsets.only(bottom: 12.h),
-                child: Container(
-                  padding: EdgeInsets.all(8.r),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey.shade300),
-                    borderRadius: BorderRadius.circular(8.r),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(12.r),
-                        child: Image.asset(
-                          'assets/logos/doctor_test.jpg',
-                          height: 70.h,
-                          width: 80.w,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(width: 12.w),
-
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'د. محمد علي',
-                              style: Styles.textStyle16Medium,
-                            ),
-                            SizedBox(height: 5.h),
-                            Text(
-                              'عيادة القاهرة -الزمالك',
-                              style: Styles.textStyle14Regular.copyWith(
-                                color: Colors.grey[700],
-                              ),
-                            ),
-                            SizedBox(height: 10.h),
-
-                            GestureDetector(
-                              onTap: () {},
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.calendar_today,
-                                    size: 18,
-                                    color: AppColors.primary,
-                                  ),
-                                  SizedBox(width: 2.w),
-                                  Text(
-                                    'احجز',
-                                    style: Styles.textStyle12SemiBold.copyWith(
-                                      color: AppColors.primary,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
+              return DoctorCard();
             },
           ),
         ],
