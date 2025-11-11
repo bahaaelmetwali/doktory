dfb22d433df20490336eee8859f942ba8ff59157
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-11-11 06:18:05 +0200

    feat : user appointments screen body ui  done

--- lib/features/user/doctor_details/presentation/widgets/user_appointments_screen_body.dart
+++ lib/features/user/doctor_details/presentation/widgets/user_appointments_screen_body.dart
@@ -9,94 +9,97 @@ class UserAppointmentsScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // بيانات تجريبية
-    final List<Map<String, String>> appointments = [
-      {
-        'doctorName': 'د. أحمد',
-        'date': '2025-11-12 10:00',
-        'status': 'pending',
-      },
-      {
-        'doctorName': 'د. سارة',
-        'date': '2025-11-13 14:00',
-        'status': 'confirmed',
-      },
-      {
-        'doctorName': 'د. كريم',
-        'date': '2025-11-14 09:30',
-        'status': 'canceled',
-      },
-    ];
-
     return Column(
       children: [
         CustomAppBar(onTap: () {}, isBack: false, headingText: 'حجوزاتي'),
-        Container(height: 10.h, color: Colors.white),
+        Container(height: 20.h, color: Colors.white),
 
         Expanded(
           child: ListView.builder(
-            itemCount: appointments.length,
+            itemCount: 6,
             itemBuilder: (context, index) {
-              final appointment = appointments[index];
-              return CustomInformationWidget(
-                content: [
-                  Row(
-                    children: [
-                      Text(
-                        'رقم الطلب',
-                        style: Styles.textStyle14SemiBold.copyWith(
-                          color: Colors.grey,
+              return Padding(
+                padding: EdgeInsets.all(8.r),
+                child: CustomInformationWidget(
+                  content: [
+                    Row(
+                      children: [
+                        Text(
+                          'اسم الدكتور',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      Spacer(),
-                      Text(
-                        '#123456',
-                        style: Styles.textStyle16Medium.copyWith(
-                          fontWeight: FontWeight.w500,
+                        Spacer(),
+                        Text(
+                          'أحمد',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.h),
-                  Row(
-                    children: [
-                      Text(
-                        'تاريخ الطلب',
-                        style: Styles.textStyle16Medium.copyWith(
-                          color: Colors.grey,
+                      ],
+                    ),
+                    SizedBox(height: 10.h),
+                    Row(
+                      children: [
+                        Text(
+                          'التخصص',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      Spacer(),
-                      Text(
-                        '28 يوليو 2024',
-                        style: Styles.textStyle16Medium.copyWith(
-                          fontWeight: FontWeight.w500,
+                        Spacer(),
+                        Text(
+                          'باطنة',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.h),
-                  Row(
-                    children: [
-                      Text(
-                        'تاريخ الطلب',
-                        style: Styles.textStyle14SemiBold.copyWith(
-                          color: Colors.grey,
+                      ],
+                    ),
+
+                    SizedBox(height: 10.h),
+                    Row(
+                      children: [
+                        Text(
+                          'تاريخ الحجز',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      Spacer(),
-                      Text(
-                        'جديد',
-                        style: Styles.textStyle14SemiBold.copyWith(
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xff34C759),
+                        Spacer(),
+                        Text(
+                          '28 يوليو 2024',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-                iconPath: 'assets/icons/information.svg',
-                title: 'بيانات الحجز',
+                      ],
+                    ),
+
+                    SizedBox(height: 10.h),
+                    Row(
+                      children: [
+                        Text(
+                          'حالة الطلب',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          'انتظار',
+                          style: Styles.textStyle14SemiBold.copyWith(
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromARGB(255, 204, 167, 33),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                  iconPath: 'assets/icons/information.svg',
+                  title: 'بيانات الحجز',
+                ),
               );
             },
           ),
