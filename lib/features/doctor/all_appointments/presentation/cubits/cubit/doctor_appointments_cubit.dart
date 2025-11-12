9d4911d141c2211297cef02b340c829508f4a9be
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-11-12 21:16:34 +0200

    solve: DoctorAppointmentsCubit edited

--- lib/features/doctor/all_appointments/presentation/cubits/cubit/doctor_appointments_cubit.dart
+++ lib/features/doctor/all_appointments/presentation/cubits/cubit/doctor_appointments_cubit.dart
@@ -1,20 +1,40 @@
 import 'package:doktory/features/doctor/all_appointments/presentation/cubits/cubit/doctor_appointments_state.dart';
 import 'package:doktory/features/doctor/all_appointments/use_cases/get_appointments_for_doctor_use_case.dart';
+import 'package:doktory/features/shared/auth/domain/usecases/get_current_user.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DoctorAppointmentsCubit extends Cubit<DoctorAppointmentsState> {
   final GetAppointmentsForDoctorUseCase getAppointmentsForDoctorUseCase;
+  final GetCurrentUserUseCase getCurrentUserUseCase;
 
-  DoctorAppointmentsCubit(this.getAppointmentsForDoctorUseCase)
-    : super(DoctorAppointmentsInitial());
+  DoctorAppointmentsCubit(
+    this.getAppointmentsForDoctorUseCase,
+    this.getCurrentUserUseCase,
+  ) : super(DoctorAppointmentsInitial());
 
-  Future<void> getAppointments(String doctorId) async {
+  Future<void> getAppointments() async {
     emit(DoctorAppointmentsLoading());
-    final result = await getAppointmentsForDoctorUseCase(doctorId);
 
-    result.fold(
-      (failure) => emit(DoctorAppointmentsFailure(failure.toString())),
-      (appointments) => emit(DoctorAppointmentsSuccess(appointments)),
+    final doctorResult = await getCurrentUserUseCase();
+
+    await doctorResult.fold(
+      (failure) async {
+        emit(DoctorAppointmentsFailure(failure.toString()));
+      },
+      (user) async {
+        if (user == null) {
+          emit(DoctorAppointmentsFailure('لم يتم العثور على بيانات المستخدم.'));
+          return;
+        }
+
+        final doctorId = user.uid;
+        final result = await getAppointmentsForDoctorUseCase(doctorId);
+
+        result.fold(
+          (failure) => emit(DoctorAppointmentsFailure(failure.toString())),
+          (appointments) => emit(DoctorAppointmentsSuccess(appointments)),
+        );
+      },
     );
   }
 }
